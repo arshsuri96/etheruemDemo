bc3e92c258efbc29e201ee6b9ccc18b82279c2a2
arshsuri96 <arshsuri96@gmail.com>
2022-06-16 14:31:57 +0530

    Create StorageFactory.sol

---
+++ StorageFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+contract StorgaeFactory {
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function creatSimpleContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStorageArray.retrieve();
+    }
+
+}
