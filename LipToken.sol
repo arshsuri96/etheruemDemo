6a473eaf823381e077539d02f4339fbf1adbe903
arshsuri96 <arshsuri96@gmail.com>
2022-06-28 17:09:44 +0530

    Update LipToken.sol

--- LipToken.sol
+++ LipToken.sol
@@ -8,15 +8,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LipToken is ERC721, Ownable {
 
-    uint256 counter = 1;
+    uint256 counter;
+
+    struct Lip{
+        string name;
+        uint256 id;
+        uint256 dna;
+        uint8 level;
+        uint8 rarity;
+    }
+
+    Lip[] public lips;
 
     constructor(string memory _name, string memory _symbol)
     ERC721(_name, _symbol)
     {}
   
-    function mintNFT(address _to)  public onlyOwner() {
-      _safeMint(_to, counter);
-      counter++;
-  }
-
+    function _cretaLip(string memory _name, uint256 _dna)  internal {
+        Lip memory newLip = Lip(_name, counter, _dna, 1, 50);
+        lips.push(newLip);
+        _safeMint(msg.sender, counter);
+        counter++;
+    }
 }
