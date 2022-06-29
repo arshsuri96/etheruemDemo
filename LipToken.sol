f9c1a33bab569f7427d9cde2935ca61299f2485e
arshsuri96 <arshsuri96@gmail.com>
2022-06-30 00:09:12 +0530

    Update LipToken.sol

--- LipToken.sol
+++ LipToken.sol
@@ -8,7 +8,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LipToken is ERC721, Ownable {
 
-    uint256 counter;
+    uint256 COUNTER;
+    uint256 fee= 0.001 ether;
 
     struct Lip{
         string name;
@@ -20,25 +21,29 @@ contract LipToken is ERC721, Ownable {
 
     Lip[] public lips;
 
+    event NewLip(address indexed owner, uint256 id, uint256 dna);
+
     constructor(string memory _name, string memory _symbol)
     ERC721(_name, _symbol)
     {}
 
-    function createRandomNumber(string memory _str) internal pure returns(uint256) {
-        uint256 randomNum = uint256(keccak256(abi.encodePacked(_str)));
-        return randomNum % 10**16;
+    function _createRandomNum(uint256 _mod) internal view returns(uint256) {
+        uint256 randomNum = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender)));
+        return randomNum % _mod;
     }
 
-    function _createLip(string memory _name, uint256 _dna) internal{
-        Lip memory newLip = Lip(_name, counter, _dna, 1, 50);
+    function _createLip(string memory _name) internal {
+        uint8 randRarity = uint8(_createRandomNum(100));
+        uint256 randDna = _createRandomNum(10**16);
+        Lip memory newLip = Lip(_name, COUNTER, randDna, 1, randRarity);
         lips.push(newLip);
-        _safeMint(msg.sender, counter);
-        counter++;
-    }
+        _safeMint(msg.sender, COUNTER);
+        emit NewLip(msg.sender, COUNTER, randDna);
+        COUNTER++;
+  }
 
     function createRandomLip(string memory _name) public {
-        uint256 randNum = createRandomNumber(_name);
-        _createLip(_name, randNum);
+        _createLip(_name);
     }
 
     function getLips() public view returns(Lip[] memory){
